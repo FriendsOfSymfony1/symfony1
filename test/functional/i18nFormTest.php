8b8b0c4e9d0f253d221f00bd513a2f9814e47057
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-11-14 14:31:05 +0000

    [1.3, 1.4] updated some more tests to use the new API
    git-svn-id: http://svn.symfony-project.com/branches/1.4@23916 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/functional/i18nFormTest.php
+++ test/functional/i18nFormTest.php
@@ -19,45 +19,61 @@ $b = new sfTestBrowser();
 // default culture (en)
 $b->
   get('/en/i18n/i18nForm')->
-  isStatusCode(200)->
-  isRequestParameter('module', 'i18n')->
-  isRequestParameter('action', 'i18nForm')->
-  isUserCulture('en')->
-  checkResponseElement('label', 'First name', array('position' => 0))->
-  checkResponseElement('label', 'Last name', array('position' => 1))->
-  checkResponseElement('label', 'Email address', array('position' => 2))->
-  checkResponseElement('td', '/Put your first name here/i', array('position' => 0))->
+  with('request')->begin()->
+    isParameter('module', 'i18n')->
+    isParameter('action', 'i18nForm')->
+  end()->
+  with('user')->isCulture('en')->
+  with('response')->begin()->
+    isStatusCode(200)->
+    checkElement('label', 'First name', array('position' => 0))->
+    checkElement('label', 'Last name', array('position' => 1))->
+    checkElement('label', 'Email address', array('position' => 2))->
+    checkElement('td', '/Put your first name here/i', array('position' => 0))->
+  end()->
   setField('i18n[email]', 'foo/bar')->
   click('Submit')->
-  checkResponseElement('ul li', 'Required.', array('position' => 0))->
-  checkResponseElement('ul li', 'foo/bar is an invalid email address', array('position' => 2))
+  with('response')->begin()->
+    checkElement('ul li', 'Required.', array('position' => 0))->
+    checkElement('ul li', 'foo/bar is an invalid email address', array('position' => 2))->
+  end()
 ;
 
 // changed culture (fr)
 $b->
   get('/fr/i18n/i18nForm')->
-  isStatusCode(200)->
-  isRequestParameter('module', 'i18n')->
-  isRequestParameter('action', 'i18nForm')->
-  isUserCulture('fr')->
-  checkResponseElement('label', 'Prénom', array('position' => 0))->
-  checkResponseElement('label', 'Nom', array('position' => 1))->
-  checkResponseElement('label', 'Adresse email', array('position' => 2))->
-  checkResponseElement('td', '/Mettez votre prénom ici/i', array('position' => 0))->
+  with('request')->begin()->
+    isParameter('module', 'i18n')->
+    isParameter('action', 'i18nForm')->
+  end()->
+  with('user')->isCulture('fr')->
+  with('response')->begin()->
+    isStatusCode(200)->
+    checkElement('label', 'Prénom', array('position' => 0))->
+    checkElement('label', 'Nom', array('position' => 1))->
+    checkElement('label', 'Adresse email', array('position' => 2))->
+    checkElement('td', '/Mettez votre prénom ici/i', array('position' => 0))->
+  end()->
   setField('i18n[email]', 'foo/bar')->
   click('Submit')->
-  checkResponseElement('ul li', 'Champ requis.', array('position' => 0))->
-  checkResponseElement('ul li', 'foo/bar est une adresse email invalide', array('position' => 2))
+  with('response')->begin()->
+    checkElement('ul li', 'Champ requis.', array('position' => 0))->
+    checkElement('ul li', 'foo/bar est une adresse email invalide', array('position' => 2))->
+  end()
 ;
 
-// forms label custoim catalogue test
+// forms label custom catalogue test
 $b->
   get('/fr/i18n/i18nCustomCatalogueForm')->
-  isStatusCode(200)->
-  isRequestParameter('module', 'i18n')->
-  isRequestParameter('action', 'i18nCustomCatalogueForm')->
-  isUserCulture('fr')->
-  checkResponseElement('label', 'Prénom!!!', array('position' => 0))->
-  checkResponseElement('label', 'Nom!!!', array('position' => 1))->
-  checkResponseElement('label', 'Adresse email!!!', array('position' => 2))
+  with('request')->begin()->
+    isParameter('module', 'i18n')->
+    isParameter('action', 'i18nCustomCatalogueForm')->
+  end()->
+  with('user')->isCulture('fr')->
+  with('response')->begin()->
+    isStatusCode(200)->
+    checkElement('label', 'Prénom!!!', array('position' => 0))->
+    checkElement('label', 'Nom!!!', array('position' => 1))->
+    checkElement('label', 'Adresse email!!!', array('position' => 2))->
+  end()
 ;
