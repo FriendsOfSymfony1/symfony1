af48d3a76466a4a3c0a50eb108f712a6b5998bdb
Kris.Wallsmith <Kris.Wallsmith@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-11-17 06:59:01 +0000

    [1.3, 1.4] cleaned up template paths shown in WDT view panel
    git-svn-id: http://svn.symfony-project.com/branches/1.4@24069 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/debug/sfWebDebugPanelView.class.php
+++ lib/debug/sfWebDebugPanelView.class.php
@@ -317,12 +317,12 @@ class sfWebDebugPanelView extends sfWebDebugPanel
 
     // application module
     $sep = preg_quote(DIRECTORY_SEPARATOR);
-    if (preg_match('#apps'.$sep.'\w+'.$sep.'modules'.$sep.'(\w+)'.$sep.'templates'.$sep.'(.*)$#', $path, $match))
+    if (preg_match('#modules'.$sep.'(\w+)'.$sep.'templates'.$sep.'(.*)$#', $path, $match))
     {
-      return $match[1].'/&hellip;/'.$match[2];
+      return $match[1].'&nbsp;&hellip;&nbsp;'.$match[2];
     }
 
-    return $path;
+    return str_replace('SF_ROOT_DIR'.DIRECTORY_SEPARATOR, '', sfDebug::shortenFilePath($path));
   }
 
   /**
