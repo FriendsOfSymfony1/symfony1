73ea5d5b8cc299e7d9966b6763616aa7be35538d
Kris.Wallsmith <Kris.Wallsmith@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-11-17 07:36:21 +0000

    [1.3, 1.4] added script to help with formatting Subversion log for CHANGELOG
    git-svn-id: http://svn.symfony-project.com/branches/1.4@24071 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ data/bin/changelog.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Outputs formatted Subversion log entries.
+ *
+ * Usage: php data/bin/changelog.php -r12345:67890 /branches/1.3
+ *
+ * @package    symfony
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @version    SVN: $Id$
+ */
+require_once dirname(__FILE__).'/../../lib/task/sfFilesystem.class.php';
+
+if (!isset($argv[1]))
+{
+  throw new Exception('You must provide a revision range (-r123:456)');
+}
+
+if (!isset($argv[2]))
+{
+  throw new Exception('You must provide a repository path (/branches/1.3)');
+}
+
+$filesystem = new sfFilesystem();
+
+list($out, $err) = $filesystem->execute('svn info --xml');
+$info = new SimpleXMLElement($out);
+
+list($out, $err) = $filesystem->execute(vsprintf('svn log %s --xml %s', array_map('escapeshellarg', array(
+  $argv[1],
+  (string) $info->entry->repository->root.$argv[2],
+))));
+$log = new SimpleXMLElement($out);
+
+foreach ($log->logentry as $logentry)
+{
+  echo sprintf(' * [%d] %s', $logentry['revision'], trim(preg_replace('/\s*\[[\d\., ]+\]\s*/', '', (string) $logentry->msg)));
+  echo PHP_EOL;
+}
