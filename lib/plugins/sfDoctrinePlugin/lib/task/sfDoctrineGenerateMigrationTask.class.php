f278b4202db3d3cde4d12a55d027fcf05c7ff97e
Kris.Wallsmith <Kris.Wallsmith@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-11-25 18:21:06 +0000

    [1.3, 1.4] removed call to deprecated sh() method
    git-svn-id: http://svn.symfony-project.com/branches/1.4@24390 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfDoctrinePlugin/lib/task/sfDoctrineGenerateMigrationTask.class.php
+++ lib/plugins/sfDoctrinePlugin/lib/task/sfDoctrineGenerateMigrationTask.class.php
@@ -85,7 +85,7 @@ EOF;
 
       if (isset($options['editor-cmd']))
       {
-        $this->getFilesystem()->sh($options['editor-cmd'].' '.escapeshellarg($file));
+        $this->getFilesystem()->execute($options['editor-cmd'].' '.escapeshellarg($file));
       }
     }
   }
