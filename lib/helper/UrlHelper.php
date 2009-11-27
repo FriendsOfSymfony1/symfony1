714fceae60fb84248cf14ecff68de318238e50f0
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-11-27 09:04:48 +0000

    [1.4] added back form_tag()
    git-svn-id: http://svn.symfony-project.com/branches/1.4@24466 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/UrlHelper.php
+++ lib/helper/UrlHelper.php
@@ -404,6 +404,50 @@ function button_to($name, $internal_uri, $options = array())
 }
 
 /**
+ * Returns an HTML <form> tag that points to a valid action, route or URL as defined by <i>$url_for_options</i>.
+ *
+ * By default, the form tag is generated in POST format, but can easily be configured along with any additional
+ * HTML parameters via the optional <i>$options</i> parameter. If you are using file uploads, be sure to set the 
+ * <i>multipart</i> option to true.
+ *
+ * <b>Options:</b>
+ * - multipart - When set to true, enctype is set to "multipart/form-data".
+ *
+ * <b>Examples:</b>
+ *   <code><?php echo form_tag('@myroute'); ?></code>
+ *   <code><?php echo form_tag('/module/action', array('name' => 'myformname', 'multipart' => true)); ?></code>
+ *
+ * @param  string $url_for_options  valid action, route or URL
+ * @param  array  $options          optional HTML parameters for the <form> tag
+ *
+ * @return string opening HTML <form> tag with options
+ */
+function form_tag($url_for_options = '', $options = array())
+{
+  $options = _parse_attributes($options);
+
+  $html_options = $options;
+
+  $html_options['method'] = isset($html_options['method']) ? strtolower($html_options['method']) : 'post';
+
+  if (_get_option($html_options, 'multipart'))
+  {
+    $html_options['enctype'] = 'multipart/form-data';
+  }
+
+  $html_options['action'] = url_for($url_for_options);
+
+  $html = '';
+  if (!in_array($html_options['method'], array('get', 'post')))
+  {
+    $html = tag('input', array('type' => 'hidden', 'name' => 'sf_method', 'value' => $html_options['method']));
+    $html_options['method'] = 'post';
+  }
+
+  return tag('form', $html_options, true).$html;
+}
+
+/**
  * Creates a <a> link tag to the given email (with href="mailto:...").
  * If null is passed as a name, the email itself will become the name.
  *
