a9312e121616453ec058e259accbe88d72b02ed3
Jonathan.Wage <Jonathan.Wage@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-12-01 02:11:40 +0000

    [1.3, 1.4] Catching Doctrine validation exceptions so you don't get internal server errors in admin generator if you use Doctrine validation
    git-svn-id: http://svn.symfony-project.com/branches/1.4@24634 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfDoctrinePlugin/data/generator/sfDoctrineModule/admin/parts/processFormAction.php
+++ lib/plugins/sfDoctrinePlugin/data/generator/sfDoctrineModule/admin/parts/processFormAction.php
@@ -5,7 +5,21 @@
     {
       $notice = $form->getObject()->isNew() ? 'The item was created successfully.' : 'The item was updated successfully.';
 
-      $<?php echo $this->getSingularName() ?> = $form->save();
+      try {
+        $<?php echo $this->getSingularName() ?> = $form->save();
+      } catch (Doctrine_Validator_Exception $e) {
+
+        $errorStack = $form->getObject()->getErrorStack();
+
+        $message = get_class($form->getObject()) . ' has ' . count($errorStack) . " field" . (count($errorStack) > 1 ?  's' : null) . " with validation errors: ";
+        foreach ($errorStack as $field => $errors) {
+            $message .= "$field (" . implode(", ", $errors) . "), ";
+        }
+        $message = trim($message, ', ');
+
+        $this->getUser()->setFlash('error', $message);
+        return sfView::SUCCESS;
+      }
 
       $this->dispatcher->notify(new sfEvent($this, 'admin.save_object', array('object' => $<?php echo $this->getSingularName() ?>)));
 
