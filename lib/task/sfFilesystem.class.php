4816f511b2bd620e7af55b20fe678a142f0e6adc
Kris.Wallsmith <Kris.Wallsmith@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-12-06 20:22:52 +0000

    [1.3, 1.4] fixed coding standards
    git-svn-id: http://svn.symfony-project.com/branches/1.4@24991 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/task/sfFilesystem.class.php
+++ lib/task/sfFilesystem.class.php
@@ -394,10 +394,13 @@ class sfFilesystem
 
   /**
    * Calculates the relative path from one to another directory.
-   * If they share no common path the absolute target dir is returned
    *
-   * @param string $from directory from that the relative path shall be calculated
-   * @param string $to target directory
+   * If the paths share no common path the absolute target dir is returned.
+   *
+   * @param string $from The directory from which to calculate the relative path
+   * @param string $to   The target directory
+   *
+   * @return string
    */ 
   protected function calculateRelativeDir($from, $to)
   {
@@ -406,45 +409,71 @@ class sfFilesystem
 
     $commonLength = 0;
     $minPathLength = min(strlen($from), strlen($to));
+
     // count how many chars the strings have in common
     for ($i = 0; $i < $minPathLength; $i++)
     {
-      if ($from[$i] != $to[$i]) break;
-      if ($from[$i] == DIRECTORY_SEPARATOR) $commonLength = $i + 1;
+      if ($from[$i] != $to[$i])
+      {
+        break;
+      }
+
+      if (DIRECTORY_SEPARATOR == $from[$i])
+      {
+        $commonLength = $i + 1;
+      }
     }
 
     if ($commonLength)
     {
       $levelUp = substr_count($from, DIRECTORY_SEPARATOR, $commonLength);
+
       // up that many level
-      $relativeDir  = str_repeat("..".DIRECTORY_SEPARATOR, $levelUp);
+      $relativeDir = str_repeat('..'.DIRECTORY_SEPARATOR, $levelUp);
+
       // down the remaining $to path
       $relativeDir .= substr($to, $commonLength);
+
       return $relativeDir;
     }
 
     return $to;
   }
 
+  /**
+   * @param string A filesystem path
+   *
+   * @return string
+   */
   protected function canonicalizePath($path)
   {
-    if (empty($path)) return '';
-    $out=array();
-    foreach( explode(DIRECTORY_SEPARATOR, $path) as $i => $fold)
+    if (empty($path))
     {
-      if ($fold=='' || $fold=='.') continue;
-      if ($fold=='..' && $i>0 && end($out)!='..')
+      return '';
+    }
+
+    $out = array();
+    foreach (explode(DIRECTORY_SEPARATOR, $path) as $i => $fold)
+    {
+      if ('' == $fold || '.' == $fold)
+      {
+        continue;
+      }
+
+      if ('..' == $fold && $i > 0 && '..' != end($out))
       {
         array_pop($out);
       }
       else
       {
-        $out[]= $fold;
+        $out[] = $fold;
       }
     }
-    $result = $path{0} == DIRECTORY_SEPARATOR ? DIRECTORY_SEPARATOR : '';
-    $result .= join(DIRECTORY_SEPARATOR, $out);
-    $result .= $path{strlen($path)-1} == DIRECTORY_SEPARATOR ? DIRECTORY_SEPARATOR : '';
+
+    $result  = DIRECTORY_SEPARATOR == $path[0] ? DIRECTORY_SEPARATOR : '';
+    $result .= implode(DIRECTORY_SEPARATOR, $out);
+    $result .= DIRECTORY_SEPARATOR == $path[strlen($path) - 1] ? DIRECTORY_SEPARATOR : '';
+
     return $result;
   }
 }
