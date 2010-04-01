706c0705e183c3f9f6b2306aed9620661e580a3e
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-04-01 14:10:24 +0000

    [1.3, 1.4] added a note about the date_format option of sfValidatorDate (closes #8316)
    git-svn-id: http://svn.symfony-project.com/branches/1.4@28959 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/validator/sfValidatorDate.class.php
+++ lib/validator/sfValidatorDate.class.php
@@ -24,6 +24,8 @@ class sfValidatorDate extends sfValidatorBase
    * Available options:
    *
    *  * date_format:             A regular expression that dates must match
+   *                             Note that the regular expression must use named subpatterns like (?P<year>)
+   *                             Working example: ~(?P<day>\d{2})/(?P<month>\d{2})/(?P<year>\d{4})~
    *  * with_time:               true if the validator must return a time, false otherwise
    *  * date_output:             The format to use when returning a date (default to Y-m-d)
    *  * datetime_output:         The format to use when returning a date with time (default to Y-m-d H:i:s)
