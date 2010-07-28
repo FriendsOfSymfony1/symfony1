9ad701dd8e458575cf5a5f8505d2eebfe33bbd74
Kris.Wallsmith <Kris.Wallsmith@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-07-28 04:37:32 +0000

    [1.3, 1.4] added a check whether doctrine is already loaded to allow use of a compiled core (closes #8917, thanks gnukix)
    git-svn-id: http://svn.symfony-project.com/branches/1.4@30445 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfDoctrinePlugin/config/sfDoctrinePluginConfiguration.class.php
+++ lib/plugins/sfDoctrinePlugin/config/sfDoctrinePluginConfiguration.class.php
@@ -37,7 +37,10 @@ class sfDoctrinePluginConfiguration extends sfPluginConfiguration
       $this->dispatcher->connect('debug.web.load_panels', array('sfWebDebugPanelDoctrine', 'listenToAddPanelEvent'));
     }
 
-    require_once sfConfig::get('sf_doctrine_dir', realpath(dirname(__FILE__).'/../lib/vendor/doctrine')).'/Doctrine/Core.php';
+    if (!class_exists('Doctrine_Core', false))
+    {
+      require_once sfConfig::get('sf_doctrine_dir', realpath(dirname(__FILE__).'/../lib/vendor/doctrine')).'/Doctrine/Core.php';
+    }
     spl_autoload_register(array('Doctrine_Core', 'autoload'));
 
     $manager = Doctrine_Manager::getInstance();
