0adb50b970f14a9d3b80dc0f53fac6c9ff656533
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2011-10-07 12:52:03 +0000

    [1.4] fixed virtual columns in sfFormFilterDoctrine (closes #9922)
    git-svn-id: http://svn.symfony-project.com/branches/1.4@33123 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfDoctrinePlugin/lib/form/sfFormFilterDoctrine.class.php
+++ lib/plugins/sfDoctrinePlugin/lib/form/sfFormFilterDoctrine.class.php
@@ -163,7 +163,7 @@ abstract class sfFormFilterDoctrine extends sfFormFilter
     $fields = $this->getFields();
 
     // add those fields that are not represented in getFields() with a null type
-    $names = array_merge($fields, array_diff(array_keys($this->validatorSchema->getFields()), array_keys($fields)));
+    $names = array_merge(array_diff(array_keys($this->validatorSchema->getFields()), array_keys($fields)));
     $fields = array_merge($fields, array_combine($names, array_fill(0, count($names), null)));
 
     foreach ($fields as $field => $type)
