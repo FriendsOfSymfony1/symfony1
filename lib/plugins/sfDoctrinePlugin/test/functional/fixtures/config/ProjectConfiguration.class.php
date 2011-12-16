d502c5809d61d3243d66de8287e9afc5f702f98a
jerome <jerome.macias@gmail.com>
2011-12-16 01:51:04 +0100

    [sfDoctrinePlugin] Removed propel reference for functional test

--- lib/plugins/sfDoctrinePlugin/test/functional/fixtures/config/ProjectConfiguration.class.php
+++ lib/plugins/sfDoctrinePlugin/test/functional/fixtures/config/ProjectConfiguration.class.php
@@ -7,7 +7,7 @@ class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
   {
-    $this->enableAllPluginsExcept(array('sfPropelPlugin'));
+    $this->enableAllPluginsExcept();
   }
 
   public function initializeDoctrine()
