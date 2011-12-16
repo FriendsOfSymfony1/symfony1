6aefb3ce1a3b55d18c1a72914bc7f7df32b96b72
jerome <jerome.macias@gmail.com>
2011-12-16 01:45:33 +0100

    Removed Propel from sfDatabaseSessionStorage

--- lib/storage/sfDatabaseSessionStorage.class.php
+++ lib/storage/sfDatabaseSessionStorage.class.php
@@ -4,7 +4,7 @@
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
  * (c) 2004-2006 Sean Kerr <sean@code-box.org>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -101,11 +101,7 @@ abstract class sfDatabaseSessionStorage extends sfSessionStorage
 
     // get the database and connection
     $databaseClass = get_class($database);
-    if($databaseClass == 'sfPropelDatabase')
-    {
-      $this->db = Propel::getConnection($database->getParameter('name'));
-    }
-    elseif($databaseClass == 'sfDoctrineDatabase')
+    if ($databaseClass == 'sfDoctrineDatabase')
     {
       $this->db = $database->getConnection();
     }
@@ -113,7 +109,7 @@ abstract class sfDatabaseSessionStorage extends sfSessionStorage
     {
       $this->db = $database->getResource();
     }
-    
+
     $this->con = $database->getConnection();
 
     if (null === $this->db && null === $this->con)
