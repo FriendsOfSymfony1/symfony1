a2785eb99965924a9450529f5103305623a10d47
jerome <jerome.macias@gmail.com>
2012-01-08 20:26:48 +0100

    Added possibility to launch bin/coverage task for only one class

--- test/bin/coverage.php
+++ test/bin/coverage.php
@@ -3,10 +3,19 @@
 /*
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+$name = '*';
+$verbose = false;
+
+if (isset($argv[1]))
+{
+  $name = $argv[1];
+  $verbose = true;
+}
 
 require_once(dirname(__FILE__).'/../../lib/vendor/lime/lime.php');
 require_once(dirname(__FILE__).'/../../lib/util/sfFinder.class.php');
@@ -15,21 +24,22 @@ $h = new lime_harness();
 $h->base_dir = realpath(dirname(__FILE__).'/..');
 
 // unit tests
-$h->register_glob($h->base_dir.'/unit/*/*Test.php');
-$h->register_glob($h->base_dir.'/unit/*/*/*Test.php');
-$h->register_glob($h->base_dir.'/../lib/plugins/*/unit/*Test.php');
-$h->register_glob($h->base_dir.'/../lib/plugins/*/unit/*/*Test.php');
+$h->register_glob(sprintf('%s/unit/*/%sTest.php', $h->base_dir, $name));
+$h->register_glob(sprintf('%s/unit/*/*/%sTest.php', $h->base_dir, $name));
+$h->register_glob(sprintf('%s/../lib/plugins/*/unit/%sTest.php', $h->base_dir, $name));
+$h->register_glob(sprintf('%s/../lib/plugins/*/unit/*/%sTest.php', $h->base_dir, $name));
 
 // functional tests
-$h->register_glob($h->base_dir.'/functional/*Test.php');
-$h->register_glob($h->base_dir.'/functional/*/*Test.php');
-$h->register_glob($h->base_dir.'/../lib/plugins/*/functional/*Test.php');
+$h->register_glob(sprintf('%s/functional/%sTest.php', $h->base_dir, $name));
+$h->register_glob(sprintf('%s/functional/*/%sTest.php', $h->base_dir, $name));
+$h->register_glob(sprintf('%s/../lib/plugins/*/functional/%sTest.php', $h->base_dir, $name));
 
 $c = new lime_coverage($h);
 $c->extension = '.class.php';
-$c->verbose = false;
+$c->verbose = $verbose;
 $c->base_dir = realpath(dirname(__FILE__).'/../../lib');
 
-$finder = sfFinder::type('file')->name('*.php')->prune('vendor')->prune('test')->prune('data');
+$finder = sfFinder::type('file')->name($name.'.class.php')->prune('vendor')->prune('test')->prune('data');
+
 $c->register($finder->in($c->base_dir));
 $c->run();
