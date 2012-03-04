f7fe330b149951b367404057afd0df26eb9e5497
jerome <jerome.macias@gmail.com>
2012-03-04 02:59:46 +0100

    Travis : installed APC before execution of check_configuration

--- test/travis.sh
+++ test/travis.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-git submodule update --init --recursive
-php data/bin/check_configuration.php
-
 wget http://pecl.php.net/get/APC-3.1.9.tgz
 tar -xzf APC-3.1.9.tgz
 sh -c "cd APC-3.1.9 && phpize && ./configure --enable-apc && make && sudo make install"
 echo "extension=apc.so
 apc.enabled = 1
 apc.enable_cli = 1" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+
+git submodule update --init --recursive
+php data/bin/check_configuration.php
