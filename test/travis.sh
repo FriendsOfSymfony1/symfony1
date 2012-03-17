5c6acdc27edcaebe967ee9f42bb5bb66194351fc
jerome <jerome.macias@gmail.com>
2012-03-18 00:50:48 +0100

    Travis : re-enabled php 5.4 with apc trunk

--- test/travis.sh
+++ test/travis.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-wget http://pecl.php.net/get/APC-3.1.9.tgz
-tar -xzf APC-3.1.9.tgz
-sh -c "cd APC-3.1.9 && phpize && ./configure --enable-apc && make && sudo make install"
+svn export http://svn.php.net/repository/pecl/apc/trunk/ apc-trunk
+sh -c "cd apc-trunk && phpize && ./configure --enable-apc && make && sudo make install"
 echo "extension=apc.so
 apc.enabled = 1
 apc.enable_cli = 1" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
