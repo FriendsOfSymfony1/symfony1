7cbbe6be6e1d7ad8314fa001865f4c25334dcc37
jerome <jerome.macias@gmail.com>
2012-04-06 01:12:31 +0200

    Optimized sfCompileConfigHandler and added unit tests

---
+++ test/unit/config/sfCompileConfigHandlerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
+
+$t = new lime_test(2);
+
+$handler = new sfCompileConfigHandler();
+$handler->initialize();
+
+$dir = dirname(__FILE__).DIRECTORY_SEPARATOR.'fixtures'.DIRECTORY_SEPARATOR.'sfCompileConfigHandler'.DIRECTORY_SEPARATOR;
+
+$t->diag('execute');
+
+sfConfig::set('sf_debug', true);
+$data = $handler->execute(array($dir.'simple.yml'));
+$t->ok(false !== strpos($data, 'class sfInflector'.PHP_EOL.'{'.PHP_EOL.'  /**'), '->execute() return complete classe codes');
+
+sfConfig::set('sf_debug', false);
+$data = $handler->execute(array($dir.'simple.yml'));
+$t->ok(false !== strpos($data, 'class sfInflector { public'), '->execute() return minified classe codes');
