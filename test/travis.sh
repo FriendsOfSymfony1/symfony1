34c66c3edf1d8b05e84dcb081811d5de3c11c66c
Jérôme Macias <jerome.macias@gmail.com>
2012-04-12 23:12:51 +0300

    Used new APC version for travis tests

--- test/travis.sh
+++ test/travis.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-svn export http://svn.php.net/repository/pecl/apc/trunk/ apc-trunk
-sh -c "cd apc-trunk && phpize && ./configure --enable-apc && make && sudo make install"
+wget http://pecl.php.net/get/APC-3.1.10.tgz
+tar -xzf APC-3.1.10.tgz
+sh -c "cd APC-3.1.10 && phpize && ./configure --enable-apc && make && sudo make install"
 echo "extension=apc.so
 apc.enabled = 1
 apc.enable_cli = 1" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
