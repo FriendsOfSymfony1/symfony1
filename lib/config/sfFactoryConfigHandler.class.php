bbe9ff61ea9138094b52eab8476b531d3fd1cc6d
Jerome Tamarelle <jtamarelle@groupe-exp.com>
2012-05-10 12:09:49 +0200

    Makes swiftmailer lib path configurable

--- lib/config/sfFactoryConfigHandler.class.php
+++ lib/config/sfFactoryConfigHandler.class.php
@@ -217,7 +217,7 @@ class sfFactoryConfigHandler extends sfYamlConfigHandler
 
         case 'mailer':
           $instances[] = sprintf(
-                        "\$swift_dir = sfConfig::get('sf_symfony_lib_dir').'/vendor/swiftmailer/lib';\n".
+                        "\$swift_dir = sfConfig::get('sf_swiftmailer_dir', sfConfig::get('sf_symfony_lib_dir').'/vendor/swiftmailer/lib');\n".
                         "require_once \$swift_dir.'/classes/Swift.php';\n".
                         "Swift::registerAutoload(\$swift_dir.'/swift_init.php');\n".
                         "\$this->setMailerConfiguration(array_merge(array('class' => sfConfig::get('sf_factory_mailer', '%s')), sfConfig::get('sf_factory_mailer_parameters', %s)));\n"
