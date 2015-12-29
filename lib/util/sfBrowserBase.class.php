3fe91ec6628e90008f1881f453933a3e6c2c3709
Marek Kilimajer <marek.kilimajer@ferratum.com>
2015-12-29 16:12:25 +0100

    sfBrowserBase - don't post unnamed submit fields - coding standard fix

--- lib/util/sfBrowserBase.class.php
+++ lib/util/sfBrowserBase.class.php
@@ -779,7 +779,8 @@ abstract class sfBrowserBase
     else if ('button' == $item->nodeName || ('input' == $item->nodeName && in_array($item->getAttribute('type'), array('submit', 'button', 'image'))))
     {
       // add the item's value to the arguments if name is provided
-      if ($item->getAttribute('name')) {
+      if ($item->getAttribute('name'))
+      {
         $this->parseArgumentAsArray($item->getAttribute('name'), $item->getAttribute('value'), $arguments);
       }
 
