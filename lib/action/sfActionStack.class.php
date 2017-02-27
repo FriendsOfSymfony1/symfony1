7154ac95fbe2dc260560f73905534bb14b1f9a09
e1himself <ivan.voskoboinyk@gmail.com>
2017-02-27 18:32:02 +0200

    Hit property type at `sfActionStack`

--- lib/action/sfActionStack.class.php
+++ lib/action/sfActionStack.class.php
@@ -4,7 +4,7 @@
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
  * (c) 2004-2006 Sean Kerr <sean@code-box.org>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -21,6 +21,7 @@
  */
 class sfActionStack
 {
+  /** @var sfActionStackEntry[] */
   protected
     $stack = array();
 
