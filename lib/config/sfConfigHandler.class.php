6450a9416cdaf601e18a830cc8a354a7bfec0997
e1himself <ivan.voskoboinyk@gmail.com>
2017-07-30 01:18:13 +0300

    Replace deprecated `create_function` with regular lambda function
    (it's okay as minimal PHP version we support is 5.3)

--- lib/config/sfConfigHandler.class.php
+++ lib/config/sfConfigHandler.class.php
@@ -75,7 +75,7 @@ abstract class sfConfigHandler
   {
     if (is_array($value))
     {
-      array_walk_recursive($value, create_function('&$value', '$value = sfToolkit::replaceConstants($value);'));
+      array_walk_recursive($value, function(& $value) { $value = sfToolkit::replaceConstants($value); });
     }
     else
     {
@@ -96,7 +96,7 @@ abstract class sfConfigHandler
   {
     if (is_array($path))
     {
-      array_walk_recursive($path, create_function('&$path', '$path = sfConfigHandler::replacePath($path);'));
+      array_walk_recursive($path, function(&$path) { $path = sfConfigHandler::replacePath($path); });
     }
     else
     {
