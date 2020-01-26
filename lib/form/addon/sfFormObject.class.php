ba1650c45fbf37816a392070da0bb0e5f908058a
Lars Erik Løvhaug <lalov@profixio.com>
2020-01-26 13:15:22 +0100

    wip

--- lib/form/addon/sfFormObject.class.php
+++ lib/form/addon/sfFormObject.class.php
@@ -167,7 +167,7 @@ abstract class sfFormObject extends BaseForm
       $con = $this->getConnection();
     }
 
-    $this->getObject()->save($con);
+    $this->getObject()->saveUsingDoctrine($con);
 
     // embedded forms
     $this->saveObjectEmbeddedForms($con);
