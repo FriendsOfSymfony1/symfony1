f1d464be5fd07903c9d0e66bd1b7f5069b2e5c13
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-17 01:18:54 +0800

    Delete lib directory

--- lib/task/generator/skeleton/app/web/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-##IP_CHECK##
-require_once(__DIR__.'/../config/ProjectConfiguration.class.php');
-
-$configuration = ProjectConfiguration::getApplicationConfiguration('##APP_NAME##', '##ENVIRONMENT##', ##IS_DEBUG##);
-sfContext::createInstance($configuration)->dispatch();
