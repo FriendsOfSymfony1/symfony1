3ae296f2af590134c353b817ec8d67dbcddb160c
Karoly Gossler <connor@connor.hu>
2022-12-13 22:32:29 +0100

    fixed php8.2 dynamic property warning
    Creation of dynamic property sfDoctrineConnectionListener::$connection is deprecated in

--- lib/plugins/sfDoctrinePlugin/lib/database/sfDoctrineConnectionListener.class.php
+++ lib/plugins/sfDoctrinePlugin/lib/database/sfDoctrineConnectionListener.class.php
@@ -20,6 +20,9 @@
  */
 class sfDoctrineConnectionListener extends Doctrine_EventListener
 {
+  public $encoding;
+  public $connection;
+
   public function __construct($connection, $encoding)
   {
     $this->connection = $connection;
