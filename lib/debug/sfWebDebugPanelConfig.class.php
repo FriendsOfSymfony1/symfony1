2462ea5a7ba497b1d566d7a1f39f20536a51dd2e
thePanz <thepanz@gmail.com>
2024-03-13 15:22:15 +0100

    Remove(legacy): delete EAccelerator cache, the extension is not existing for php v7.4
    See: https://github.com/eaccelerator/eaccelerator, where the following
    message is shown:
    
    > This project is deprecated and does not work with anything newer than PHP 5.3.
    > Please use a recent version of PHP with OPcache instead!

--- lib/debug/sfWebDebugPanelConfig.class.php
+++ lib/debug/sfWebDebugPanelConfig.class.php
@@ -34,7 +34,6 @@ class sfWebDebugPanelConfig extends sfWebDebugPanel
             'cache' => sfConfig::get('sf_cache') ? 'on' : 'off',
             'compression' => sfConfig::get('sf_compressed') ? 'on' : 'off',
             'tokenizer' => function_exists('token_get_all') ? 'on' : 'off',
-            'eaccelerator' => extension_loaded('eaccelerator') && ini_get('eaccelerator.enable') ? 'on' : 'off',
             'apc' => extension_loaded('apc') && ini_get('apc.enabled') ? 'on' : 'off',
             'xcache' => extension_loaded('xcache') && ini_get('xcache.cacher') ? 'on' : 'off',
         );
