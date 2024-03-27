4cf370593ee11f014c810a9e9b8dc9e9cbb520b3
connor <connor@connor.hu>
2024-03-27 09:12:51 +0100

    dropped pear releasing process

--- bin/release.php
+++ bin/release.php
@@ -21,34 +21,4 @@ $version = $argv[1];
 
 echo sprintf("Releasing symfony version \"%s\".\n", $version);
 
-if (is_file('package.xml')) {
-    $filesystem->remove(getcwd().DIRECTORY_SEPARATOR.'package.xml');
-}
-
-$filesystem->copy(getcwd().'/package.xml.tmpl', getcwd().'/package.xml');
-
-// add class files
-$finder = sfFinder::type('file')->relative();
-$xml_classes = '';
-$dirs = ['lib' => 'php', 'data' => 'data'];
-foreach ($dirs as $dir => $role) {
-    $class_files = $finder->in($dir);
-    foreach ($class_files as $file) {
-        $xml_classes .= '<file role="'.$role.'" baseinstalldir="symfony" install-as="'.$file.'" name="'.$dir.'/'.$file.'" />'."\n";
-    }
-}
-
-// replace tokens
-$filesystem->replaceTokens(getcwd().DIRECTORY_SEPARATOR.'package.xml', '##', '##', [
-    'SYMFONY_VERSION' => $version,
-    'CURRENT_DATE' => date('Y-m-d'),
-    'CLASS_FILES' => $xml_classes,
-    'STABILITY' => $stability,
-]);
-
-list($results) = $filesystem->execute('pear package');
-echo $results;
-
-$filesystem->remove(getcwd().DIRECTORY_SEPARATOR.'package.xml');
-
 exit(0);
