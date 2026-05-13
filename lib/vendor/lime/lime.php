9771e596b6b1caf69b0924f99bb19968e5e2fb31
Johannes Tyra <johannes.tyra@braindata.de>
2026-05-13 15:26:20 +0200

    PHP 8.4: replace utf8_encode in lime JUnit reporter
    utf8_encode() has been deprecated since PHP 8.2. The lime JUnit
    reporter uses it to escape test names for the XML output. Switch
    to mb_convert_encoding($s, 'UTF-8', 'ISO-8859-1'), the documented
    migration path. PR #393 added PHP 8.5 support to lime.php but did
    not address this remaining call.
    
    Under PHP 8.4.7 the deprecation reads:
        Function utf8_encode() is deprecated since 8.2,
        visit the php.net documentation for various alternatives

--- lib/vendor/lime/lime.php
+++ lib/vendor/lime/lime.php
@@ -103,7 +103,7 @@ class lime_test
       foreach ($result['tests'] as $test)
       {
         $testsuite->appendChild($testcase = $dom->createElement('testcase'));
-        $testcase->setAttribute('name', utf8_encode($test['message']));
+        $testcase->setAttribute('name', mb_convert_encoding($test['message'], 'UTF-8', 'ISO-8859-1'));
         $testcase->setAttribute('file', $test['file']);
         $testcase->setAttribute('line', $test['line']);
         $testcase->setAttribute('assertions', 1);
